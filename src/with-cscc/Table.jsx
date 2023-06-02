b7a8081e3068c623d7fff662d4583ea01d687d6a
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-06-02 10:34:50 +0400

    finish mark up

--- src/with-cscc/Table.jsx
+++ src/with-cscc/Table.jsx
@@ -16,9 +16,15 @@ export default function Table(){
                 </div>
             </div>
             <div className="info-wrapper"> 
-                <h5>Name</h5>
+                <h6>Name</h6>
                 <p>Excuse</p>
                 <span><p>count</p></span>
+            </div>
+            <div className="shame-wrapper">
+                <h4 className="full-name">Brad Simons</h4>
+                <span className="spanForTextWidth"><p>Remember when you were young you shone like the sun. Shine on you</p></span>
+                <span className="spanForCount"><p>3</p></span>
+                <span className="spanForButton"><button className="buttonForIncrease">Increase</button></span>
             </div>
         </div>
     )
